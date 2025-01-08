e05762cd24ed906471056a6e88ae3e73e1b77395
couldbecassie <iamnotalan3@gmail.com>
2025-01-08 09:25:39 +0100

    meow

--- homemade/logout.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('logout-btn').addEventListener('click', function() {
-    // Clear session storage
-    sessionStorage.removeItem('loggedInUser');
-    
-    // Redirect to login page
-    window.location.href = 'RNGgpus.html'; // Replace 'index.html' with the path to your login page
-});
